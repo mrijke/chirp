9dc6176a4011e5ebe116dc413c12a5dbb0b9e99f
Maarten Rijke <maartenrijke@gmail.com>
2022-03-12 14:41:03 -0800

    Add disabled styles to Button

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -9,11 +9,13 @@ export const Button: React.FC<IButtonProps> = ({
   label,
   badge,
   className,
+  disabled,
   ...buttonProps
 }) => (
   <button
     type="button"
-    className={`inline-flex py-2 px-5 w-fit h-10 text-gray-300 hover:text-white bg-gray-800 hover:bg-gray-600 rounded-lg ring-gray-700 ${className}`}
+    className={`inline-flex py-2 px-5 w-fit h-10 text-gray-300 hover:text-white bg-gray-800 hover:bg-gray-600 rounded-lg ring-gray-700 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400 ${className}`}
+    disabled={disabled}
     {...buttonProps}
   >
     {label}
